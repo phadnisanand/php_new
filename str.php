33ae036d2fce7a8315830a4f8a12d01f943cbc72
Anand <phadnisanandhyd@gmail.com>
2024-12-13 09:14:24 +0530

    php

--- str.php
+++ str.php
@@ -2,20 +2,20 @@
 // https://www.cloudways.com/blog/php-8/#nullsafe
 // https://www.geeksforgeeks.org/php-str_starts_with-function/?ref=ml_lbp
 
-	$string = 'GFG is awesome';
-	$endsWith = 'some';
+$string = 'GFG is awesome';
+$endsWith = 'some';
 
-	$result = str_ends_with($string, $endsWith) ? 'is' : 'is not';
+$result = str_ends_with($string, $endsWith) ? 'is' : 'is not';
 
-	echo "The string \"$string\" $result ending with $endsWith";
-	
-	echo '<br />';
-	$name = 'Saurabh Singh'; 
-    $beginsWith = 'Saurabh'; 
-  
-    $result = str_starts_with($name, $beginsWith) ? 'is' : 'is not'; 
-  
-    echo "The string \"$name\" $result starting with $beginsWith"; 
-  
+echo "The string \"$string\" $result ending with $endsWith";
+
+echo '<br />';
+$name = 'Saurabh Singh'; 
+$beginsWith = 'Saurabh'; 
+
+$result = str_starts_with($name, $beginsWith) ? 'is' : 'is not'; 
+
+echo "The string \"$name\" $result starting with $beginsWith"; 
+
 
 ?>
