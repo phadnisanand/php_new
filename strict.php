3c676287d8242633d96069b008c28afd71cfb949
Anand <phadnisanandhyd@gmail.com>
2025-03-24 10:30:48 +0530

    stricy

---
+++ strict.php
@@ -0,0 +1,10 @@
+<?php
+  declare (strict_types=1);
+   function addition(int $x,int  $y) {
+      echo "First number: $x Second number: $y Addition: " . $x+$y;
+   }
+
+   $x = 10;
+   $y = 20;
+   addition($x, $y);
+?>
